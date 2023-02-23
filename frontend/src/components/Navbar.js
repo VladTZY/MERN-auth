4735a53022a3b874033cc9ea8f093c500bd91d06
VladTZY <tiganilavlad@gmail.com>
2023-02-23 09:31:26 -0800

    MERN APP

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
+
+const Navbar = () => {
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+
+  const handleClick = () => {
+    logout();
+  };
+
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Workout Buddy</h1>
+        </Link>
+        <nav>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Log out</button>
+            </div>
+          )}
+          {!user && (
+            <div>
+              <Link to="/login">
+                <h1>Login</h1>
+              </Link>
+              <Link to="/signup">
+                <h1>Signup</h1>
+              </Link>
+            </div>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
